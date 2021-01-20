ef7bf7ed964b8665180e09a0a8dcc25dd17b0e48
KaueLui <ofckaue@gmail.com>
2021-01-19 21:14:57 -0300

    Colunas e Métodos para Game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    use HasFactory;
+     use HasFactory;
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'developer',
+        'description',
+    ];
 }
